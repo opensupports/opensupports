5acd086dd8073c0e2556a807a53fd9052b247171
Ivan Diaz <ivan@opensupports.com>
2015-10-13 21:25:09 -0300

    [Ivan Diaz] - Update reactDFS lib

--- src/utils/react-dfs.js
+++ src/utils/react-dfs.js
@@ -1,4 +1,5 @@
 import React     from 'react/addons';
+import _         from 'lodash';
 
 var reactDFS = function (children, visitFunction) {
     var stack = [];
@@ -12,8 +13,31 @@ var reactDFS = function (children, visitFunction) {
         React.Children.forEach(element.props.children, child => tempChilds.push(child));
 
         visitFunction(element);
-        stack.concat(tempChilds.reverse());
+        stack = stack.concat(tempChilds.reverse());
     }
 };
 
-export default reactDFS;
+var renderChildrenWithProps = function(children, mapFunction) {
+    if (typeof children !== 'object' || children === null) {
+        return children;
+    }
+
+    return React.Children.map(children, function (child) {
+        var props = mapFunction(child);
+
+        if (typeof child !== 'object' || child === null) {
+            return child;
+        }
+
+        if (!_.isEmpty(props)) {
+            return React.cloneElement(child, props, child.props && child.props.children);
+        } else {
+            return React.cloneElement(child, {}, renderChildrenWithProps(child.props && child.props.children, mapFunction));
+        }
+    }.bind(this));
+};
+
+export default {
+    renderChildrenWithProps,
+    reactDFS
+};
