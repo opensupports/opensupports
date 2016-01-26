bd5e14d5a185689aefb10c88d4d90e46d3db4f00
Ivan Diaz <ivan@opensupports.com>
2016-01-26 10:39:36 -0300

    [Ivan Diaz] - Fix indentation of unit test [skip ci]

--- src/core-components/__tests__/button-test.js
+++ src/core-components/__tests__/button-test.js
@@ -3,33 +3,34 @@ jest.dontMock('../button.js');
 import React from 'react/addons';
 import Button from '../button.js';
 
-var TestUtils = React.addons.TestUtils;
+let TestUtils = React.addons.TestUtils;
 
-describe('Button', () => {
-	it('should render children', () => {
-		var button = TestUtils.renderIntoDocument(
-			<Button>
-				testcontent
-			</Button>
-		);
+describe('Button', function () {
+    it('should render children', function () {
+        let button = TestUtils.renderIntoDocument(
+            <Button>
+                testcontent
+            </Button>
+        );
 
-		expect(button.getDOMNode().textContent).toEqual('testcontent');
-	});
+        expect(button.getDOMNode().textContent).toEqual('testcontent');
+    });
 
-	it('should add passed types to class', () => {
-		var types = [
-			'primary',
+    it('should add passed types to class', function () {
+        let types = [
+            'primary',
             'clean',
             'link'
-		];
+        ];
 
-		types.forEach((type) => {
-			var button = TestUtils.renderIntoDocument(
-				<Button type={type}>
-					testcontent
-				</Button>
-			);
-			expect(button.getDOMNode().getAttribute('class')).toContain('button-' + type);
-		});
-	});
+        types.forEach(function (type) {
+            let button = TestUtils.renderIntoDocument(
+                <Button type={type}>
+                    testcontent
+                </Button>
+            );
+
+            expect(button.getDOMNode().getAttribute('class')).toContain('button-' + type);
+        });
+    });
 });
