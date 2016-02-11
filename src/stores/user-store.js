3c56dbbcb7795716ca1e836767ccad01fd82b39b
Ivan Diaz <ivan@opensupports.com>
2016-02-11 15:32:59 -0300

    [Ivan Diaz] - #8 - Replace tab character with 4 spaces [skip ci]

--- src/stores/user-store.js
+++ src/stores/user-store.js
@@ -3,24 +3,24 @@ const API = require('lib-app/api-call');
 
 const UserActions = require('actions/user-actions');
 
-let UserStore = Reflux.createStore({
+const UserStore = Reflux.createStore({
 
-	init() {
-		this.user = null;
-		this.hasBeenChecked = false;
+    init() {
+        this.user = null;
+        this.hasBeenChecked = false;
 
-		this.listenTo(UserActions.checkLoginStatus, this.checkLoginStatus);
-		this.listenTo(UserActions.login, this.loginUser);
-		this.listenTo(UserActions.logout, this.logoutUser);
-	},
+        this.listenTo(UserActions.checkLoginStatus, this.checkLoginStatus);
+        this.listenTo(UserActions.login, this.loginUser);
+        this.listenTo(UserActions.logout, this.logoutUser);
+    },
 
     loginUser(loginData) {
-		API.call('user/login', loginData, result => {
-			console.log(result);
+        API.call('user/login', loginData, result => {
+            console.log(result);
 
-			API.setConfig(result.userId, result.token);
+            API.setConfig(result.userId, result.token);
         });
-	}
+    }
 });
 
 export default UserStore;
