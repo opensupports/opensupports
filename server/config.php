a6b7ec145ed768b3b3d79e3e4ce5c5737e892980
Ivan Diaz <ivan@opensupports.com>
2016-05-14 16:47:31 +0000

    [Ivan Diaz] - Remove default

--- server/config.php
+++ server/config.php
@@ -1,5 +1,5 @@
 <?php
 $mysql_host       = 'localhost';
-$mysql_user       = $_ENV['MYSQL_USER'] || 'root';
-$mysql_password   = $_ENV['MYSQL_PASSWORD'] || '';
+$mysql_user       = $_ENV['MYSQL_USER'];
+$mysql_password   = $_ENV['MYSQL_PASSWORD'];
 $mysql_database   = 'development';
