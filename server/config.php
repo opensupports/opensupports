e83c0328a28950f4f425fbc9f59cb3cac8563367
Ivan Diaz <ivan@opensupports.com>
2016-05-14 15:32:44 -0300

    [Ivan Diaz] -Add default values if environment variables are not defined

--- server/config.php
+++ server/config.php
@@ -1,5 +1,10 @@
 <?php
-$mysql_host       = 'localhost';
-$mysql_user       = $_ENV['MYSQL_USER'];
-$mysql_password   = $_ENV['MYSQL_PASSWORD'];
-$mysql_database   = 'development';
+$env['MYSQL_SERVER']   = getenv('MYSQL_SERVER');
+$env['MYSQL_USER']     = getenv('MYSQL_USER');
+$env['MYSQL_PASSWORD'] = getenv('MYSQL_PASSWORD');
+$env['MYSQL_DATABASE'] = getenv('MYSQL_DATABASE');
+
+$mysql_host       = ($env['MYSQL_SERVER'])   ? $env['MYSQL_SERVER']   : 'localhost';
+$mysql_user       = ($env['MYSQL_USER'])     ? $env['MYSQL_USER']     : 'root';
+$mysql_password   = ($env['MYSQL_PASSWORD']) ? $env['MYSQL_PASSWORD'] : '';
+$mysql_database   = ($env['MYSQL_DATABASE']) ? $env['MYSQL_DATABASE'] : 'development';
