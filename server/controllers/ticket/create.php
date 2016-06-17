86fc4ab0b65ae5be8f31eb987a9297081d05c271
Ivan Diaz <ivan@opensupports.com>
2016-06-17 17:16:10 -0300

    [AntonyAntonio] - Add initial 'ticket/create' controller [skip ci]

--- server/controllers/ticket/create.php
+++ server/controllers/ticket/create.php
@@ -4,6 +4,44 @@ class CreateController extends Controller {
     const PATH = '/create';
 
     public function handler(){
-        Response::respondError('TEST');
+        $title = Controller::request('title');
+        $content = Controller::request('content');
+        $departmentId = Controller::request('departmentId');
+        $language = Controller::request('language');
+        if (strlen($title)<3 ){
+            Response::respondError('title is so short');
+            return;
+        }
+        if (strlen($title)>30){
+            Response::respondError('title is so long');
+            return;
+        }
+        if (strlen($content)<5){
+            Response::respondError('content is so short');
+            return;
+        }
+        if (strlen($content)>100){
+            Response::respondError('content is so long');
+            return;
+        }
+        $ticket = new Ticket();
+        $ticket->setProperties(array(
+            'ticketId' => '',
+            'title' => $title,
+            'content' => $content,
+            'language' => $language,
+            'department' => $departmentId,
+            'file' => '',
+            'date' => date("F j, Y, g:i a"),
+            'unread' => false,
+            'closed' => false,
+            'author' => '',
+            'owner'=> '',
+            'ownComments' => []
+        ));
+
+        $ticket->store();
+        Response::respondSuccess();
+
     }
 }
