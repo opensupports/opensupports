47571ec068e99b1a39d14e8ec12f23cee08ed1c1
ivan <ivan@opensupports.com>
2016-07-06 12:54:48 -0300

    Ivan - OS-18 - Create dashboard pages placeholders [skip ci]

---
+++ client/src/app/main/dashboard/dashboard-list-articles/dashboard-list-articles-page.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const DashboardListArticlesPage = React.createClass({
+
+    render() {
+        return (
+            <div>
+                DASHBOARD ARTICLES LIST
+            </div>
+        );
+    }
+});
+
+export default DashboardListArticlesPage;
