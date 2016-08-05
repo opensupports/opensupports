26ac580ed05694b46d55a535da772b2d6693945b
ivan <ivan@opensupports.com>
2016-08-05 20:48:15 -0300

    Ivan - Add signup fixtures, signup API call, signup messages, styling updates [skip ci]

--- client/src/data/languages/en.js
+++ client/src/data/languages/en.js
@@ -7,15 +7,19 @@ export default {
     'RECOVER_SENT': 'An email with recover instructions has been sent.',
     'NEW_PASSWORD': 'New password',
     'REPEAT_NEW_PASSWORD': 'Repeat new password',
-    'VALID_RECOVER': 'Password recovered successfully',
-    'INVALID_RECOVER': 'Invalid recover data',
     'BACK_LOGIN_FORM': 'Back to login form',
-    'EMAIL_NOT_EXIST': 'Email does not exist',
 
     //ERRORS
+    'EMAIL_NOT_EXIST': 'Email does not exist',
     'ERROR_EMPTY': 'Invalid value',
     'ERROR_PASSWORD': 'Invalid password',
     'ERROR_NAME': 'Invalid name',
     'ERROR_EMAIL': 'Invalid email',
-    'PASSWORD_NOT_MATCH': 'Password does not match'
+    'PASSWORD_NOT_MATCH': 'Password does not match',
+    'INVALID_RECOVER': 'Invalid recover data',
+
+    //MESSAGES
+    'SIGNUP_SUCCESS': 'You have registered successfully in our support system.',
+    'VALID_RECOVER': 'Password recovered successfully',
+    'EMAIL_EXISTS': 'Email already exists, please try to log in or recover password'
 };
