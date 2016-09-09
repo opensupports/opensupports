8f5852a0d0842b513937752cef1f2c418e91c589
ivan <ivan@opensupports.com>
2016-09-09 16:27:31 -0300

    Ivan - Frontend - Add animation to modal [skip ci]

--- client/src/core-components/modal.js
+++ client/src/core-components/modal.js
@@ -1,5 +1,7 @@
 import React from 'react';
 
+import {Motion, spring} from 'react-motion';
+
 class Modal extends React.Component {
     static propTypes = {
         content: React.PropTypes.node
@@ -8,11 +10,37 @@ class Modal extends React.Component {
     render() {
         return (
             <div className="modal">
-                <div className="modal__content">
-                    {this.props.content}
-                </div>
+                <Motion {...this.getAnimations()}>
+                    {this.renderContent.bind(this)}
+                </Motion>
             </div>
         );
+    }
+
+    getAnimations() {
+        return {
+            defaultStyle: {
+                scale: spring(0.7),
+                fade: spring(0)
+            },
+            style: {
+                scale: spring(1),
+                fade: spring(1)
+            }
+        }
+    }
+
+    renderContent(animation) {
+        const style = {
+            transform: 'scale(' + animation.scale + ')',
+            opacity: animation.fade
+        };
+
+        return (
+            <div className="modal__content" style={style}>
+                {this.props.content}
+            </div>
+        )
     }
 }
 
