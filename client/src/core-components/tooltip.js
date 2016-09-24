bf7513740c78e210884c2e82c3a6f2c4323c1e00
ivan <ivan@opensupports.com>
2016-09-23 21:42:31 -0300

    Ivan - dev3 deploy implementation fixes

--- client/src/core-components/tooltip.js
+++ client/src/core-components/tooltip.js
@@ -5,19 +5,21 @@ class Tooltip extends React.Component {
     
     static propTypes = {
         children: React.PropTypes.node,
-        content: React.PropTypes.node
+        content: React.PropTypes.node,
+        openOnHover: React.PropTypes.bool
     };
 
-    constructor (props){
-        super(props);
-        this.state = {show : false};
-    }
+    state = {
+        show : false
+    };
 
     render (){
         return (
-            <div className="tooltip" >
+            <div {...this.getProps()}>
                 {(this.state.show) ? this.renderAnimatedMessage() : null}
-                <div className="tooltip__children" onClick={this.onClick.bind(this)}>{this.props.children}</div>
+                <div {...this.getChildrenProps()}>
+                    {this.props.children}
+                </div>
             </div>
         );
     }
@@ -40,6 +42,41 @@ class Tooltip extends React.Component {
             </div>
         )
     }
+
+    getProps(){
+        let props = {};
+
+        props.className = 'tooltip';
+
+        if(this.props.openOnHover) {
+            props.onMouseOver = this.onMouseOver.bind(this);
+            props.onMouseOut = this.onMouseOut.bind(this);
+        }
+
+        return props;
+    }
+
+    getChildrenProps() {
+        let props = {};
+        props.className= 'tooltip__children';
+
+        if (this.props.openOnHover) {
+            props.onClick= this.onClick.bind(this);
+        }
+        return props;
+    }
+
+    onMouseOver() {
+        this.setState({
+            show:true
+        })
+    }
+    onMouseOut() {
+        this.setState({
+            show:false
+        })
+    }
+
     onClick(){
         if (this.state.show) {
             this.setState({show : false});
