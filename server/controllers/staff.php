4f127b1d214db5163c620c2eeb413923178179eb
Ivan Diaz <ivan@opensupports.com>
2016-09-30 18:47:04 -0300

    Ivan - Add text editor to text area [skip ci]

--- server/controllers/staff.php
+++ server/controllers/staff.php
@@ -1,9 +1,11 @@
 <?php
 require_once 'staff/get.php';
+require_once 'staff/assign.php';
 
 $systemControllerGroup = new ControllerGroup();
 $systemControllerGroup->setGroupPath('/staff');
 
 $systemControllerGroup->addController(new GetStaffController);
+$systemControllerGroup->addController(new AssignStaffController);
 
 $systemControllerGroup->finalize();
