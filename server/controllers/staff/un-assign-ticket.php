5034ba71a3236cfdb4470b9baa2f67b6dbdd900c
ivan <ivan@opensupports.com>
2016-10-14 16:12:54 -0300

    guillermo- path- staff-un-assign-ticket [skip ci]

---
+++ server/controllers/staff/un-assign-ticket.php
@@ -0,0 +1,36 @@
+<?php
+use Respect\Validation\Validator as DataValidator;
+DataValidator::with('CustomValidations', true);
+
+class UnAssignStaffController extends Controller {
+    const PATH = '/un-assign-ticket';
+
+    public function validations() {
+        return [
+            'permission' => 'staff_1',
+            'requestData' => [
+                'ticketNumber' => [
+                    'validation' => DataValidator::validTicketNumber(),
+                    'error' => ERRORS::INVALID_TICKET
+                ]
+            ]
+        ];
+    }
+
+    public function handler() {
+        $ticketNumber = Controller::request('ticketNumber');
+        $user = Controller::getLoggedUser();
+        $ticket = Ticket::getByTicketNumber($ticketNumber);
+
+        if($ticket->owner->id == $user->id) {
+            $user->sharedTicketList->remove($ticket);
+            $user->store();
+            $ticket->owner = null;
+            $ticket->store();
+            Response::respondSuccess();
+        } else {
+            Response::respondError(ERRORS::NO_PERMISSION);
+            return;
+        }
+    }
+}
