59099a3bbf5dcf8be9f5986fcdcf75bab5bf2e07
ivan <ivan@opensupports.com>
2016-10-14 21:59:11 -0300

    guillermo- path- staff-get-ticket [skip ci]

---
+++ tests/staff/get-tickets.rb
@@ -0,0 +1,26 @@
+describe '/staff/get-tickets' do
+    request('/user/logout')
+    Scripts.login($staff[:email], $staff[:password], true)
+
+    it 'should get ticket list' do
+
+        ticket = $database.getRow('ticket', 1 , 'id')
+        request('/staff/assign-ticket', {
+            ticketNumber: ticket['ticket_number'],
+            csrf_userid: $csrf_userid,
+            csrf_token: $csrf_token
+        })
+        ticket = $database.getRow('ticket', 2 , 'id')
+        request('/staff/assign-ticket', {
+            ticketNumber: ticket['ticket_number'],
+            csrf_userid: $csrf_userid,
+            csrf_token: $csrf_token
+        })
+        result = request('/staff/get-tickets', {
+            csrf_userid: $csrf_userid,
+            csrf_token: $csrf_token
+        })
+
+        (result['status']).should.equal('success')
+    end
+end
