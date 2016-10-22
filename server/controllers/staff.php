7693f030f062351d47e97aece1871f301042d359
AntonyAntonio <guillermo@opensupports.com>
2016-10-22 00:40:56 -0300

    Guillermo -   staff-get-all-tickets[skip ci]

--- server/controllers/staff.php
+++ server/controllers/staff.php
@@ -4,6 +4,7 @@ require_once 'staff/assign-ticket.php';
 require_once 'staff/un-assign-ticket.php';
 require_once 'staff/get-tickets.php';
 require_once 'staff/get-new-tickets.php';
+require_once 'staff/get-all-tickets.php';
 
 $systemControllerGroup = new ControllerGroup();
 $systemControllerGroup->setGroupPath('/staff');
@@ -13,5 +14,7 @@ $systemControllerGroup->addController(new AssignStaffController);
 $systemControllerGroup->addController(new UnAssignStaffController);
 $systemControllerGroup->addController(new GetTicketStaffController);
 $systemControllerGroup->addController(new GetNewTicketsStaffController);
+$systemControllerGroup->addController(new GetAllTicketsStaffController);
+
 
 $systemControllerGroup->finalize();
