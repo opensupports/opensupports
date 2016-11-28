55e6f344c3145b9c3970e32ea8976601f761ab7d
ivan <ivan@opensupports.com>
2016-11-28 19:56:42 -0300

    Ivan - Add user view [skip ci]

--- client/src/data/languages/en.js
+++ client/src/data/languages/en.js
@@ -59,6 +59,7 @@ export default {
     'SAVE': 'Save',
     'DISCARD_CHANGES': 'Discard changes',
     'DELETE': 'Delete',
+    'DELETE_AND_BAN': 'Delete and ban',
     'LANGUAGE': 'Language',
     'OWNER': 'Owner',
     'OWNED': 'Owned',
@@ -78,7 +79,8 @@ export default {
     'NAME': 'Name',
     'SIGNUP_DATE': 'Sign up date',
     'SEARCH_USERS': 'Search users...',
-
+    'USER_VIEW_TITLE': 'User #{userId}',
+    
     //VIEW DESCRIPTIONS
     'CREATE_TICKET_DESCRIPTION': 'This is a form for creating tickets. Fill the form and send us your issues/doubts/suggestions. Our support system will answer it as soon as possible.',
     'TICKET_LIST_DESCRIPTION': 'Here you can find a list of all tickets you have sent to our support team.',
@@ -92,6 +94,8 @@ export default {
     'ALL_TICKETS_DESCRIPTION': 'Here you can view the tickets of the departments you are assigned.',
     'TICKET_VIEW_DESCRIPTION': 'This ticket has been sent by a customer. Here you can respond or assign the ticket',
     'LIST_USERS_DESCRIPTION': 'This is the list of users that are registered in this platform. You can search for someone in particular, delete it or ban it.',
+    'USER_VIEW_DESCRIPTION': 'Here you can find all the information about an user and all the tickets sent by the user. You can also delete or ban it.',
+    'DELETE_USER_DESCRIPTION': 'The user will not be able to log in aging and all its tickets will be erased. Also, the email can not be used any more.',
 
     //ERRORS
     'EMAIL_OR_PASSWORD': 'Email or password invalid',
@@ -106,6 +110,7 @@ export default {
     'INVALID_RECOVER': 'Invalid recover data',
     'TICKET_SENT_ERROR': 'An error occurred while trying to create the ticket.',
     'NO_PERMISSION': 'You\'ve no permission to access to this page.',
+    'INVALID_USER': 'User id is invalid',
 
     //MESSAGES
     'SIGNUP_SUCCESS': 'You have registered successfully in our support system.',
