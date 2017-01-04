d7cd5086bd1dae3e3be4bada1f859b9a894363b8
AntonyAntonio <guillermo@opensupports.com>
2017-01-03 21:06:45 -0300

    Guillermo -   tests get/edit/recover-mail-templates [skip ci]

---
+++ tests/system/get-mail-templates.rb
@@ -0,0 +1,15 @@
+describe'system/get-mail-templates' do
+        request('/user/logout')
+        Scripts.login($staff[:email], $staff[:password], true)
+
+        it 'should get mail templates' do
+            result= request('/system/get-mail-templates', {
+                csrf_userid: $csrf_userid,
+                csrf_token: $csrf_token
+            })
+
+            (result['status']).should.equal('success')
+
+            (result['data'].size).should.equal(10)
+        end
+end
