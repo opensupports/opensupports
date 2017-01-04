8e05054ef4a7bbe757fb00765696970e685ba9ef
Ivan Diaz <ivan@opensupports.com>
2017-01-04 17:42:51 -0300

    Ivan - Remove debugger [skip ci]

--- client/src/lib-app/i18n.js
+++ client/src/lib-app/i18n.js
@@ -8,7 +8,6 @@ let mf = new MessageFormat('en');
 let i18n = function (key, params = null) {
     let i18nKey = i18nData(key, store.getState().config.language);
     let message = mf.compile(i18nKey);
-    debugger;
 
     return message(params);
 };
