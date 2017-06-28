d14aeaeabca39cb5773256daa97a01dcfb987830
ivan <ivan@opensupports.com>
2017-06-28 02:55:32 -0300

    Ivan - Add  #37 changes: redirect to admin when install, add link to documentation in admin panel

--- client/src/app/main/main-layout-footer.js
+++ client/src/app/main/main-layout-footer.js
@@ -1,16 +1,42 @@
 import React              from 'react';
+import {connect}          from 'react-redux';
+import classNames         from 'classnames';
 
 class MainLayoutFooter extends React.Component {
 
     render() {
         return (
-            <div className="main-layout-footer">
-                <div className="main-layout-footer--powered">
-                    Powered by <a className="main-layout-footer--os-link" href="http://www.opensupports.com/" target="_blank">OpenSupports</a>
+            <div className={this.getClass()}>
+                {(this.props.adminPanelOpened) ? this.renderExtraLinks() : null}
+                <div className="main-layout-footer__powered">
+                    Powered by <a className="main-layout-footer__os-link" href="http://www.opensupports.com/" target="_blank">OpenSupports</a>
                 </div>
             </div>
         );
     }
+
+    renderExtraLinks() {
+        return (
+            <div className="main-layout-footer__extra-links">
+                <a className="main-layout-footer__extra-link" href="http://www.opensupports.com/documentation/" target="_blank">Documentation</a>
+                <span> | </span>
+                <a className="main-layout-footer__extra-link" href="http://www.opensupports.com/download/#donation" target="_blank">Donate</a>
+            </div>
+        );
+    }
+
+    getClass() {
+        let classes = {
+            'main-layout-footer': true,
+            'main-layout-footer_admin-panel': this.props.adminPanelOpened
+        };
+
+        return classNames(classes);
+    }
 }
 
-export default MainLayoutFooter;
+export default connect((store) => {
+    return {
+        adminPanelOpened: store.session.staff
+    };
+})(MainLayoutFooter);
