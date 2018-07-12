787bd9a42bbfddef9095ba520ffe9c75e1bb268e
Guillermo <guillermo@opensupports.com>
2018-07-12 02:08:37 -0300

    test of feature #101

--- tests/ticket/close.rb
+++ tests/ticket/close.rb
@@ -2,8 +2,6 @@ describe '/ticket/close' do
     request('/user/logout')
     Scripts.login($staff[:email], $staff[:password], true)
 
-    #TODO: DO THINGS
-
     it 'should close a ticket if everything is okey' do
         ticket = $database.getRow('ticket', 1 , 'id')
 
@@ -21,5 +19,29 @@ describe '/ticket/close' do
 
         lastLog = $database.getLastRow('log')
         (lastLog['type']).should.equal('CLOSE')
+
+        request('/user/logout')
+        Scripts.createUser('closer@os4.com','closer','Closer')
+        Scripts.login('closer@os4.com','closer')
+        Scripts.createTicket('tickettoclose')
+
+        ticket = $database.getRow('ticket', 'tickettoclose', 'title')
+
+        result = request('/ticket/close', {
+            ticketNumber: ticket['ticket_number'],
+            csrf_userid: $csrf_userid,
+            csrf_token: $csrf_token
+
+        })
+
+        (result['status']).should.equal('success')
+
+        ticket = $database.getRow('ticket', 'tickettoclose', 'title')
+        (ticket['closed']).should.equal('1')
+        (ticket['unread']).should.equal('1')
+
+        lastLog = $database.getLastRow('log')
+        (lastLog['type']).should.equal('CLOSE')
+
     end
 end
