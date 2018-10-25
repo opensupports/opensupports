6a922cbe3533eee441d642639ef5a2a921e1f91e
zmryoyo <44424755+zmryoyo@users.noreply.github.com>
2018-10-25 13:43:33 +0700

    Update language-list.js

--- client/src/data/language-list.js
+++ client/src/data/language-list.js
@@ -12,6 +12,7 @@ import italianLanguage    from 'data/languages/it';
 import brazilLanguage     from 'data/languages/br';
 import greekLanguage      from 'data/languages/gr';
 import dutchLanguage      from 'data/languages/nl';
+import vietnamLanguage    from 'data/languages/vn';
 
 export default {
     'en': {
@@ -69,5 +70,9 @@ export default {
     'nl': {
         name: 'Dutch',
         data: dutchLanguage
+    },
+    'vn': {
+        name: 'Vietnamese',
+        data: vietnamLanguage
     },
 };
