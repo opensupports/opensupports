099dd5a5a0a9d126d087acbdd43a1c1804e9c1aa
Ivan Diaz <ivan@opensupports.com>
2018-10-29 19:32:03 -0300

    Fix ticket delete test issues

--- tests/staff/delete.rb
+++ tests/staff/delete.rb
@@ -1,28 +1,30 @@
 describe'/staff/delete' do
     request('/user/logout')
     Scripts.login($staff[:email], $staff[:password], true)
+    @staffId = $database.getRow('staff','littlelannister@opensupports.com','email')['id']
 
     it 'should delete staff member' do
         result= request('/staff/delete', {
             csrf_userid: $csrf_userid,
             csrf_token: $csrf_token,
-            staffId: 3
+            staffId: @staffId
         })
 
         (result['status']).should.equal('success')
 
-        row = $database.getRow('staff', 3, 'id')
+        row = $database.getRow('staff', @staffId, 'id')
         (row).should.equal(nil)
 
         row = $database.getRow('department', 1, 'id')
         (row['owners']).should.equal('3')
 
     end
+
     it 'should fail delete if staff member is does not exist' do
-        result= request('/staff/delete', {
+        result = request('/staff/delete', {
             csrf_userid: $csrf_userid,
             csrf_token: $csrf_token,
-            staffId: 3
+            staffId: @staffId
         })
 
         (result['status']).should.equal('fail')
--- tests/ticket/delete.rb
+++ tests/ticket/delete.rb
@@ -1,52 +1,46 @@
 describe '/ticket/delete' do
-    request('/user/logout')
-    Scripts.login($staff[:email], $staff[:password], true)
-    Scripts.createTicket('tickettodelete')
-    Scripts.createTicket('tickettodelete4')
 
-    # it 'should delete ticket if it is not assigned and  is logged a staff lvl 3 ' do
-    #
-    #
-    #     ticket = $database.getRow('ticket', 'tickettodelete', 'title')
-    #
-    #     request('/staff/add', {
-    #         csrf_userid: $csrf_userid,
-    #         csrf_token: $csrf_token,
-    #         name: 'Ned Stark',
-    #         password: 'headless',
-    #         email: 'ned@opensupports.com',
-    #         level: 3,
-    #         profilePic: '',
-    #         departments: '[1]'
-    #     })
-    #
-    #     request('/user/logout')
-    #
-    #     Scripts.login('ned@opensupports.com', 'headless', true)
-    #
-    #     result = request('/ticket/delete', {
-    #         ticketNumber: ticket['ticket_number'],
-    #         csrf_userid: $csrf_userid,
-    #         csrf_token: $csrf_token
-    #     })
-    #
-    #     (result['status']).should.equal('success')
-    # end
+    it 'should delete ticket if it is not assigned and is logged a staff lvl 3 ' do
+        request('/user/logout')
+        Scripts.login($staff[:email], $staff[:password], true)
+        Scripts.createTicket('ticket_to_delete')
+        ticket = $database.getRow('ticket', 'ticket_to_delete', 'title')
+
+        request('/staff/add', {
+            csrf_userid: $csrf_userid,
+            csrf_token: $csrf_token,
+            name: 'Ned Stark',
+            password: 'headless',
+            email: 'ned@opensupports.com',
+            level: 3,
+            profilePic: '',
+            departments: '[1]'
+        })
+
+        request('/user/logout')
+        Scripts.login('ned@opensupports.com', 'headless', true)
+
+        result = request('/ticket/delete', {
+            ticketNumber: ticket['ticket_number'],
+            csrf_userid: $csrf_userid,
+            csrf_token: $csrf_token
+        })
+
+        (result['status']).should.equal('success')
+    end
 
     it 'should delete ticket if it is yours and it is not assigned' do
       request('/user/logout')
       Scripts.createUser('deleter@opensupports.com', 'deleterpassword', 'Delter')
       Scripts.login('deleter@opensupports.com', 'deleterpassword')
 
-      Scripts.createTicket('tickettodelete2')
-      ticket = $database.getRow('ticket', 'tickettodelete2', 'title');
-
+      Scripts.createTicket('ticket_to_delete_2')
+      ticket = $database.getRow('ticket', 'ticket_to_delete_2', 'title');
       result = request('/ticket/delete', {
           ticketNumber: ticket['ticket_number'],
           csrf_userid: $csrf_userid,
           csrf_token: $csrf_token
       })
-      puts result
       (result['status']).should.equal('success')
     end
 
@@ -54,8 +48,8 @@ describe '/ticket/delete' do
       request('/user/logout')
       Scripts.login('deleter@opensupports.com', 'deleterpassword')
 
-      Scripts.createTicket('tickettodelete3')
-      ticket = $database.getRow('ticket', 'tickettodelete3', 'title');
+      Scripts.createTicket('ticket_to_delete_3')
+      ticket = $database.getRow('ticket', 'ticket_to_delete_3', 'title');
 
       request('/user/logout')
       Scripts.login($staff[:email], $staff[:password], true)
@@ -76,14 +70,15 @@ describe '/ticket/delete' do
       })
 
       (result['status']).should.equal('fail')
+      (result['message']).should.equal('NO_PERMISSION')
     end
 
     it 'should not delete ticket if the staff logged is not lvl 3' do
          request('/user/logout')
-
          Scripts.login($staff[:email], $staff[:password], true)
+         Scripts.createTicket('ticket_to_delete_4')
 
-         ticket = $database.getRow('ticket', 'tickettodelete4', 'title');
+         ticket = $database.getRow('ticket', 'ticket_to_delete_4', 'title');
 
          request('/staff/add', {
              csrf_userid: $csrf_userid,
@@ -106,15 +101,14 @@ describe '/ticket/delete' do
          })
 
          (result['status']).should.equal('fail')
+         (result['message']).should.equal('NO_PERMISSION')
 
+         request('/user/logout')
+         Scripts.login($staff[:email], $staff[:password], true)
+         staff = $database.getRow('staff', 'ned@opensupports.com', 'email')
+         Scripts.deleteStaff(staff['id'])
+
+         staff = $database.getRow('staff', 'uselessstaff@opensupports.com', 'email')
+         Scripts.deleteStaff(staff['id'])
     end
-
-    request('/user/logout')
-    Scripts.login($staff[:email], $staff[:password], true)
-    staff = $database.getRow('staff', 'headless', 'password')
-    Scripts.deleteStaff(staff['id'])
-
-    staff = $database.getRow('staff', 'theyaregonnafireme', 'password')
-    Scripts.deleteStaff(staff['id'])
-
 end
