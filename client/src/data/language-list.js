eec6479f49fc0d3774d2ea9e4b97c72c6859b272
Bozsóki Dániel <bozsokidaniel5@gmail.com>
2019-02-15 20:28:53 +0100

    Update language-list.js

--- client/src/data/language-list.js
+++ client/src/data/language-list.js
@@ -12,6 +12,7 @@ import italianLanguage    from 'data/languages/it';
 import brazilLanguage     from 'data/languages/br';
 import greekLanguage      from 'data/languages/gr';
 import dutchLanguage      from 'data/languages/nl';
+import hungarianLanguage  from 'data/languages/hu';
 
 export default {
     'en': {
@@ -69,5 +70,9 @@ export default {
     'nl': {
         name: 'Dutch',
         data: dutchLanguage
+    },
+    'hu': {
+        name: 'Hungarian',
+        data: hungarianLanguage
     },
 };
