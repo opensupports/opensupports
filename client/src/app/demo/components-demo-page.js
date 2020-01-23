a6c3cfeba96ca9ddb0fd2a3ab159d7559efea4f2
LautaroCesso <lautaro_cesso@hotmail.com>
2020-01-22 22:14:27 -0300

    Restore demo-page

--- client/src/app/demo/components-demo-page.js
+++ client/src/app/demo/components-demo-page.js
@@ -1,76 +1,268 @@
-import Autocomplete from 'core-components/autocomplete';
+'use strict';
 
 const React = require('react');
+// const LineChart = require("react-chartjs-2").Line;
 const _ = require('lodash');
+const DocumentTitle = require('react-document-title');
 
-const color = [
-    'red',
-    'cyan',
-    'blue',
-    'green',
+const ModalContainer = require('app-components/modal-container');
+const AreYouSure = require('app-components/are-you-sure');
+
+const Button = require('core-components/button');
+const Input = require('core-components/input');
+const Checkbox = require('core-components/checkbox');
+const Widget = require('core-components/widget');
+const DropDown = require('core-components/drop-down');
+const Menu = require('core-components/menu');
+const Tooltip = require('core-components/tooltip');
+const Table = require('core-components/table');
+const InfoTooltip = require('core-components/info-tooltip');
+const TagSelector = require('core-components/tag-selector');
+
+function rand(min, max, num) {
+    var rtn = [];
+    while (rtn.length < num) {
+        rtn.push((Math.random() * (max - min)) + min);
+    }
+    return rtn;
+}
+
+let chartData = {
+    labels: ["January", "February", "March", "April", "May", "June"],
+    datasets: [
+        {
+            label: "My Second dataset",
+            fill: false,
+            pointColor: "rgba(151,187,205,1)",
+            pointStrokeColor: "#fff",
+            pointHighlightFill: "#fff",
+            pointHighlightStroke: "rgba(151,187,205,1)",
+            borderWidth: 3,
+            data: rand(32, 100, 6),
+            pointRadius: 0
+        },
+        {
+            label: "My Second dataset",
+            fill: false,
+            pointColor: "rgba(151,187,205,1)",
+            pointStrokeColor: "#fff",
+            pointHighlightFill: "#fff",
+            pointHighlightStroke: "rgba(151,187,205,1)",
+            borderWidth: 3,
+            data: rand(32, 100, 6)
+        }
+    ]
+};
+let chartOptions = {};
+let dropDownItems = [{content: 'English'}, {content: 'Spanish'}, {content: 'German'}, {content: 'Portuguese'}, {content: 'Japanese'}];
+let secondaryMenuItems = [
+    {content: 'My Tickets', icon: 'file-text'},
+    {content: 'New Ticket', icon: 'plus'},
+    {content: 'Articles', icon: 'book'},
+    {content: 'Edit Profile', icon: 'pencil'},
+    {content: 'Close Session', icon: 'lock'}
 ];
 
-let countries = ["Afghanistan","Åland Islands","Albania","Algeria","American Samoa","AndorrA","Angola","Anguilla","Antarctica","Antigua and Barbuda","Argentina","Armenia","Aruba","Australia","Austria","Azerbaijan","Bahamas","Bahrain","Bangladesh","Barbados","Belarus","Belgium","Belize","Benin","Bermuda","Bhutan","Bolivia","Bosnia and Herzegovina","Botswana","Bouvet Island","Brazil","British Indian Ocean Territory","Brunei Darussalam","Bulgaria","Burkina Faso","Burundi","Cambodia","Cameroon","Canada","Cape Verde","Cayman Islands","Central African Republic","Chad","Chile","China","Christmas Island","Cocos (Keeling) Islands","Colombia","Comoros","Congo","Congo, The Democratic Republic of the","Cook Islands","Costa Rica","Cote D'Ivoire","Croatia","Cuba","Cyprus","Czech Republic","Denmark","Djibouti","Dominica","Dominican Republic","Ecuador","Egypt","El Salvador","Equatorial Guinea","Eritrea","Estonia","Ethiopia","Falkland Islands (Malvinas)","Faroe Islands","Fiji","Finland","France","French Guiana","French Polynesia","French Southern Territories","Gabon","Gambia","Georgia","Germany","Ghana","Gibraltar","Greece","Greenland","Grenada","Guadeloupe","Guam","Guatemala","Guernsey","Guinea","Guinea-Bissau","Guyana","Haiti","Heard Island and Mcdonald Islands","Holy See (Vatican City State)","Honduras","Hong Kong","Hungary","Iceland","India","Indonesia","Iran, Islamic Republic Of","Iraq","Ireland","Isle of Man","Israel","Italy","Jamaica","Japan","Jersey","Jordan","Kazakhstan","Kenya","Kiribati","Korea, Democratic People'S Republic of","Korea, Republic of","Kuwait","Kyrgyzstan","Lao People'S Democratic Republic","Latvia","Lebanon","Lesotho","Liberia","Libyan Arab Jamahiriya","Liechtenstein","Lithuania","Luxembourg","Macao","Macedonia, The Former Yugoslav Republic of","Madagascar","Malawi","Malaysia","Maldives","Mali","Malta","Marshall Islands","Martinique","Mauritania","Mauritius","Mayotte","Mexico","Micronesia, Federated States of","Moldova, Republic of","Monaco","Mongolia","Montserrat","Morocco","Mozambique","Myanmar","Namibia","Nauru","Nepal","Netherlands","Netherlands Antilles","New Caledonia","New Zealand","Nicaragua","Niger","Nigeria","Niue","Norfolk Island","Northern Mariana Islands","Norway","Oman","Pakistan","Palau","Palestinian Territory, Occupied","Panama","Papua New Guinea","Paraguay","Peru","Philippines","Pitcairn","Poland","Portugal","Puerto Rico","Qatar","Reunion","Romania","Russian Federation","RWANDA","Saint Helena","Saint Kitts and Nevis","Saint Lucia","Saint Pierre and Miquelon","Saint Vincent and the Grenadines","Samoa","San Marino","Sao Tome and Principe","Saudi Arabia","Senegal","Serbia and Montenegro","Seychelles","Sierra Leone","Singapore","Slovakia","Slovenia","Solomon Islands","Somalia","South Africa","South Georgia and the South Sandwich Islands","Spain","Sri Lanka","Sudan","Suriname","Svalbard and Jan Mayen","Swaziland","Sweden","Switzerland","Syrian Arab Republic","Taiwan, Province of China","Tajikistan","Tanzania, United Republic of","Thailand","Timor-Leste","Togo","Tokelau","Tonga","Trinidad and Tobago","Tunisia","Turkey","Turkmenistan","Turks and Caicos Islands","Tuvalu","Uganda","Ukraine","United Arab Emirates","United Kingdom","United States","United States Minor Outlying Islands","Uruguay","Uzbekistan","Vanuatu","Venezuela","Viet Nam","Virgin Islands, British","Virgin Islands, U.S.","Wallis and Futuna","Western Sahara","Yemen","Zambia","Zimbabwe"];
-countries = countries.map((name, index) => {
-    return {
-        id: index,
-        name: name.toLowerCase(),
-        content: name,
-        color: color[_.random(0, color.length-1)],
-    }
-});
+let DemoPage = React.createClass({
 
-const searchApi = (query, blacklist = []) => {
-    const data = countries.filter(x => !_.includes(blacklist, x.id));
+	propTypes: {
+		currentUser: React.PropTypes.object.isRequired
+	},
 
-    return new Promise((res,rej) => {
-        setTimeout(function () {
-            const result = data.filter(item => _.includes(item.name, query));
-            res(result.slice(0, 10));
-        }, query == 'brazilq' ? 100 : 50);
-    });
-};
+	elements: [
+		{
+			title: 'Primary Button',
+			render: (
+				<Button type="primary">Sign up</Button>
+			)
+		},
+        {
+            title: 'Tag selector',
+            render: (
+                <TagSelector
+                    items={[
+                        {name: 'tag1', color: 'blue'},
+                        {name: 'suggestion', color: '#ff6900'},
+                        {name: 'tag3', color: 'red'},
+                        {name: 'tag4', color: 'green'},
+                        {name: 'bug', color: '#eb144c'},
+                    ]}
+                    values={['suggestion','bug', 'tag4']}
+                    onRemoveClick={(e)  => console.log('deleted click', e)}
+                    onTagSelected={(e)  => console.log('selected click', e)}
+                />
+            )
+        },
+        {
+            title: 'Input',
+            render: (
+                <Input placeholder="placeholder" error="No anda"/>
+            )
+        },
+        {
+            title: 'Input wrapped in a label',
+            render: (
+                <Input placeholder="placeholder" label="This is a label" icon="user"/>
+            )
+        },
+        {
+            title: 'Checkbox',
+            render: (
+                <Checkbox label="Remember me" value={true} />
+            )
+        },
+        {
+            title: 'Widget',
+            render: (
+                <Widget style={{ width: 324 }}>
+                    <h2>Register here!</h2>
 
-class DemoPage extends React.Component {
-   
-    state = {
-        selectedList: [
-            {id: 47, name: 'asdasdasd', content: 'asdasdasd.', color: 'red'},
-            {id: 48, name: '123123123', content: '123123123.', color: 'blue'},
-            {id: 49, name: 'hola', content: 'hola', color: 'green'},
-        ],
-        selectedList2: [],
-    }
-    
+                    <Button type="primary">SIGN UP</Button>
+                </Widget>
+            )
+        },
+        {
+            title: 'Primary Menu',
+            render: (
+                <Menu items={dropDownItems} />
+            )
+        },
+        {
+            title: 'Secondary Menu',
+            render: (
+                <Menu items={secondaryMenuItems} type="secondary"/>
+            )
+        },
+        {
+            title: 'Navigation Menu',
+            render: (
+                <Menu items={secondaryMenuItems} type="navigation"/>
+            )
+        },
+        {
+            title: 'Horizontal Menu',
+            render: (
+                <Menu items={secondaryMenuItems.slice(0, 3)} type="horizontal"/>
+            )
+        },
+        {
+            title: 'HorizontalList Menu',
+            render: (
+                <Menu items={dropDownItems.slice(0, 3)} type="horizontal-list"/>
+            )
+        },
+        {
+            title: 'DropDown',
+            render: (
+                <DropDown items={dropDownItems} onChange={function (index) { console.log('changed to ' + index); }} />
+            )
+        },
+        {
+            title: 'Tooltip',
+            render: (
+                <div>
+                    <Tooltip content="mensaje mensa jemensajemens ajem ensaje  nsaje adicionals">
+                        hola
+                    </Tooltip>
+                </div>
+            )
+        },
+        {
+            title: 'ModalTrigger',
+            render: (
+                <Button onClick={function () {
+                    AreYouSure.openModal('I confirm I want to perform this action.', ()=> {alert('yes');}, 'secure')
+                }}>
+                    Open Modal
+                </Button>
+            )
+        },
+        {
+            title: 'ModalTrigger Large',
+            render: (
+                <Button onClick={function () {
+                    ModalContainer.openModal(
+                        <div>
+                            {_.range(1, 60).map(() => <div>Some modal content</div>)}
+                        </div>
+                    );
+                }}>
+                    Open Large Modal
+                </Button>
+            )
+        },
+        {
+            title: 'Table',
+            render: (
+                <Table headers={[
+                    {value:'Title First', key: 'title1'},
+                    {value:'Title Second', key: 'title2'}
+                ]} rows={[
+                    {title1: 'Row1', title2: 'Example', n: 1},
+                    {title1: 'Row2', title2: 'Example', n: 2},
+                    {title1: 'Row3', title2: 'Example', n: 3},
+                    {title1: 'Row4', title2: 'Example', n: 4},
+                    {title1: 'Row5', title2: 'Example', n: 5},
+                    {title1: 'Row6', title2: 'Example', n: 6},
+                    {title1: 'Row7', title2: 'Example', n: 7},
+                    {title1: 'Row8', title2: 'Example', n: 8},
+                    {title1: 'Row9', title2: 'Example', n: 9},
+                    {title1: 'Row10', title2: 'Example', n: 10},
+                    {title1: 'Row11', title2: 'Example', n: 11},
+                    {title1: 'Row12', title2: 'Example', n: 12},
+                    {title1: 'Row13', title2: 'Example', n: 13},
+                    {title1: 'Row14', title2: 'Example', n: 14}
+                ]} pageSize={3} comp={function (a, b) {
+                    let ans = 0;
+                    if(a.title1 < b.title1)
+                        ans = -1;
+                    else if(a.title1 > b.title1)
+                        ans = 1;
+                    return ans;
+                }}/>
+            )
+        },
+        {
+            title: 'InfoTooltip',
+            render: (
+                <InfoTooltip type="warning" text="No staff member is assigned to this department." />
+            )
+        },
+        {
+            title: 'LineChart',
+            // render: (
+            //     <LineChart data={chartData} options={chartOptions} width="600" height="250" />
+            // ),
+            render: (
+                null
+            )
+        }
+	],
+
 	render() {
-        let itemsList = [
-            {id: 45, name: 'lautaro', content: 'Lautaro.', color: 'gray'},
-            {id: 46, name: 'dsafa', content: 'dsafa', color: 'black'},
-            {id: 47, name: 'asdasdasd', content: 'asdasdasd.', color: 'red'},
-            {id: 48, name: '123123123', content: '123123123.', color: 'blue'},
-            {id: 49, name: 'hola', content: 'hola', color: 'green'},
-        ];
-
-        return (
-            <div>
-                <Autocomplete
-                    items={itemsList}
-                    values={this.state.selectedList}
-                    onChange={selectedList => this.setState({selectedList: selectedList})} />
-                <button onClick={() =>
-                this.setState({
-                    selectedList: [], })
-                    }>clear</button>
-                <Autocomplete
-                    values={this.state.selectedList2}
-                    getItemListFromQuery={searchApi}
-                    onChange={selectedList => this.setState({selectedList2: selectedList})} />
-                <button onClick={() => {
-                    this.setState({selectedList2: []});
-                    }}>clear</button>
-            </div>
+		return (
+			<DocumentTitle title="Demo Page">
+				<section className="demo-page">
+					{this.renderElements()}
+				</section>
+			</DocumentTitle>
 		);
+	},
+
+	renderElements: function () {
+		return this.elements.map((element) => {
+			return (
+				<div className="demo-element col-md-4">
+					<h4>
+					{element.title}
+					</h4>
+					<div class="demo-element--example">
+					{element.render}
+					</div>
+				</div>
+			);
+		});
 	}
-}
+});
 
 export default DemoPage;
