f4b3ea2a6546c7f6129a97cc23e7afb316bd8b76
Guillermo Giuliana <guillermogiuliana@hotmail.com>
2020-01-24 15:18:05 -0300

    seach valid staff to validownersid

--- server/libs/validations/validOwnersId.php
+++ server/libs/validations/validOwnersId.php
@@ -7,15 +7,13 @@ use Respect\Validation\Rules\AbstractRule;
 class ValidOwnersId extends AbstractRule {
 
     public function validate($owners) {
-    	if(is_array(json_decode($owners))){
-			foreach (json_decode($owners) as $owner) {
-                
-                $author = \Ticket::getDataStore($owner, "owner_id");
-				
-				if($author->isNull()) return false; 
-			}
-	        return true;
-	    }
-	    return false;
+        if(is_array(json_decode($owners))){
+            foreach (json_decode($owners) as $owner) {  
+                $author = \Staff::getDataStore($owner);
+                if($author->isNull()) return false; 
+            }
+            return true;
+        }
+        return false;
     }
 }
