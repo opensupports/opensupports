c6c1a57c57c1543191577bd94311324e4c8e7024
Guillermo Giuliana <guillermogiuliana@hotmail.com>
2020-03-02 17:19:14 -0300

    ruby tests get authors path

--- server/libs/validations/validBlackList.php
+++ server/libs/validations/validBlackList.php
@@ -9,8 +9,8 @@ class ValidBlackList extends AbstractRule {
     public function validate($blackList) {
     	if(is_array(json_decode($blackList))){
 			foreach (json_decode($blackList) as $item) {
-                if(!$item->id && !$item->staff) return false;
-				if($item->staff !== 0 && $item->staff !== 1) return false;
+                if(!$item->id || !$item->staff) return false;
+				if($item->staff != 0 && $item->staff != 1) return false;
                 if(!is_numeric($item->id)) return false;
 			}
 	        return true;
