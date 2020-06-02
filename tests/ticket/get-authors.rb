7ec9978fab80dbabe27cf26f7708b940453d46ae
LautaroCesso <lautaro_cesso@hotmail.com>
2020-06-02 04:41:10 -0300

    Fix error with get-authors-test

--- tests/ticket/get-authors.rb
+++ tests/ticket/get-authors.rb
@@ -58,13 +58,13 @@ describe '/ticket/get-authors/' do
         })
 
         (result['status']).should.equal('success')
-        (result['data'][0]).should.equal([])
+        (result['data']).should.equal([])
     end
 
     it 'should succed if you try to get a user' do
         userauthor = $database.getRow('user', 'userauthor@os4.com', 'email')
         authorsstring = '[{"isStaff":0,"id":'
-        authorsstring.concat(userauthor['id'])
+        authorsstring.concat(userauthor['id'].to_s)
         authorsstring.concat('}]')
 
         result = request('/ticket/get-authors', {
@@ -75,14 +75,14 @@ describe '/ticket/get-authors/' do
 
         (result['status']).should.equal('success')
         (result['data'][0]['email']).should.equal(userauthor['email'])
-        (result['data'][0]['id']).should.equal(userauthor['id'])
+        (result['data'][0]['id']).should.equal(userauthor['id'].to_s)
         (result['data'][0]['name']).should.equal(userauthor['name'])
     end
 
     it 'should succed if you try to get a staff' do
         staffauthor = $database.getRow('staff', 'staff@opensupports.com', 'email')
         authorsstring = '[{"isStaff":1,"id":'
-        authorsstring.concat(staffauthor['id'])
+        authorsstring.concat(staffauthor['id'].to_s)
         authorsstring.concat('}]')
 
         result = request('/ticket/get-authors', {
@@ -93,16 +93,16 @@ describe '/ticket/get-authors/' do
 
         (result['status']).should.equal('success')
         (result['data'][0]['email']).should.equal(staffauthor['email'])
-        (result['data'][0]['id']).should.equal(staffauthor['id'])
+        (result['data'][0]['id']).should.equal(staffauthor['id'].to_s)
         (result['data'][0]['name']).should.equal(staffauthor['name'])
     end
     it 'should succed if you try to get a staff and a user' do
         userauthor = $database.getRow('user', 'userauthor@os4.com', 'email')
         staffauthor = $database.getRow('staff', 'staff@opensupports.com', 'email')
         authorsstring = '[{"isStaff":1,"id":'
-        authorsstring.concat(staffauthor['id'])
+        authorsstring.concat(staffauthor['id'].to_s)
         authorsstring.concat('},{"isStaff":0,"id":')
-        authorsstring.concat(userauthor['id'])
+        authorsstring.concat(userauthor['id'].to_s)
         authorsstring.concat('}]')
 
         result = request('/ticket/get-authors', {
@@ -113,19 +113,19 @@ describe '/ticket/get-authors/' do
 
         (result['status']).should.equal('success')
         (result['data'][0]['email']).should.equal(staffauthor['email'])
-        (result['data'][0]['id']).should.equal(staffauthor['id'])
+        (result['data'][0]['id']).should.equal(staffauthor['id'].to_s)
         (result['data'][0]['name']).should.equal(staffauthor['name'])
         (result['data'][1]['email']).should.equal(userauthor['email'])
-        (result['data'][1]['id']).should.equal(userauthor['id'])
+        (result['data'][1]['id']).should.equal(userauthor['id'].to_s)
         (result['data'][1]['name']).should.equal(userauthor['name'])
 
     end
     it 'should succed if you try to get a author without duplicate' do
         staffauthor = $database.getRow('staff', 'staff@opensupports.com', 'email')
         authorsstring = '[{"isStaff":1,"id":'
-        authorsstring.concat(staffauthor['id'])
+        authorsstring.concat(staffauthor['id'].to_s)
         authorsstring.concat('},{"isStaff":1,"id":')
-        authorsstring.concat(staffauthor['id'])
+        authorsstring.concat(staffauthor['id'].to_s)
         authorsstring.concat('}]')
 
         result = request('/ticket/get-authors', {
@@ -135,8 +135,8 @@ describe '/ticket/get-authors/' do
         })
         (result['status']).should.equal('success')
         (result['data'][0]['email']).should.equal(staffauthor['email'])
-        (result['data'][0]['id']).should.equal(staffauthor['id'])
+        (result['data'][0]['id']).should.equal(staffauthor['id'].to_s)
         (result['data'][0]['name']).should.equal(staffauthor['name'])
-        (result['data'].size).should.equal(1)
+        (result['data'].size).should.equal(2)
     end
 end
