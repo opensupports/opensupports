ef0e920fac0edcc130ec80b0ca1c06056518dd3b
Maximiliano Redigonda <maxiredigonda@gmail.com>
2020-10-09 10:57:21 -0300

    Factors out function to create ticket and adds test

--- tests/system/get-stats.rb
+++ tests/system/get-stats.rb
@@ -11,10 +11,26 @@ describe '/system/stats/' do
         Scripts.login($staff[:email], $staff[:password], true)
     end
 
+    @dateRangeBefore2000 = "[197001010000,200001010000]"
+    @currTicket = 9901
+
+    def createTicket()
+        result = request('/ticket/create', {
+            title: "Stats Ticket ##{@currTicket}: Title",
+            content: "Stats Ticket ##{@currTicket}: Content",
+            departmentId: 1,
+            language: 'en',
+            csrf_userid: $csrf_userid,
+            csrf_token: $csrf_token
+        })
+        $database.query("UPDATE ticket SET date=199701010000 ORDER BY id DESC LIMIT 1")
+        @currTicket += 1
+    end
+
     it 'should report no stats before year 2000' do
         asStaff()
         result = request('/system/stats', {
-            dateRange: "[197001010000,200001010000]",
+            dateRange: @dateRangeBefore2000,
             csrf_userid: $csrf_userid,
             csrf_token: $csrf_token
         })
@@ -36,19 +52,10 @@ describe '/system/stats/' do
 
     it 'should update number of created tickets after a ticket is created' do
         asUser()
-        result = request('/ticket/create', {
-            title: 'Stats Ticket #1: Title',
-            content: 'Stats Ticket #1: Content',
-            departmentId: 1,
-            language: 'en',
-            csrf_userid: $csrf_userid,
-            csrf_token: $csrf_token
-        })
-        $database.query("UPDATE ticket SET date=199701010000 ORDER BY id DESC LIMIT 1")
-
+        createTicket()
         asStaff()
         result = request('/system/stats', {
-            dateRange: "[197001010000,200001010000]",
+            dateRange: @dateRangeBefore2000,
             csrf_userid: $csrf_userid,
             csrf_token: $csrf_token
         })
@@ -56,5 +63,27 @@ describe '/system/stats/' do
         (result['data']['created']).should.equal(1)
         (result['data']['open']).should.equal(1)
         (result['data']['closed']).should.equal(0)
+    end
+
+    it 'should update number of closed tickets after a ticket is closed' do
+        asStaff()
+        ticket = $database.getLastRow('ticket')
+
+        result = request('/ticket/close', {
+            ticketNumber: ticket['ticket_number'],
+            csrf_userid: $csrf_userid,
+            csrf_token: $csrf_token
+        })
+        (result['status']).should.equal('success')
+
+        result = request('/system/stats', {
+            dateRange: @dateRangeBefore2000,
+            csrf_userid: $csrf_userid,
+            csrf_token: $csrf_token
+        })
+
+        (result['data']['created']).should.equal(1)
+        (result['data']['open']).should.equal(0)
+        (result['data']['closed']).should.equal(1)
     end
 end
