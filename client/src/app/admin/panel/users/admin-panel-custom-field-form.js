41c014dd0db062962ce7aee2a0f4f06e2e90d814
Guillermo Giuliana <guillermogiuliana@hotmail.com>
2021-09-13 17:39:05 -0300

    delete listener and add onkeydown prop

--- client/src/app/admin/panel/users/admin-panel-custom-field-form.js
+++ client/src/app/admin/panel/users/admin-panel-custom-field-form.js
@@ -41,7 +41,8 @@ class AdminPanelCustomFieldForm extends React.Component {
                         loading={loading}
                         values={addForm}
                         onChange={this.onAddFormChange.bind(this)}
-                        onSubmit={this.onSubmit.bind(this)}>
+                        onSubmit={this.onSubmit.bind(this)}
+                        onKeyDown={(event) => { if(event.key == 'Enter') event.preventDefault()}}>
                         <FormField name="name" validation="NAME" label={i18n('NAME')} field="input" fieldProps={{size: 'large'}} required/>
                         <FormField name="description" label={i18n('FIELD_DESCRIPTION')} field="input" fieldProps={{size: 'large'}}/>
                         <FormField name="type" label={i18n('TYPE')} field="select" fieldProps={{size: 'large', items: [{content: i18n('TEXT_INPUT')}, {content: i18n('SELECT_INPUT')}]}} required/>
@@ -94,11 +95,6 @@ class AdminPanelCustomFieldForm extends React.Component {
     }
 
     onDeleteOptionClick(index, event) {
-        
-        document.addEventListener('keydown', (event) => {
-            event.key == 'Enter' ? event.preventDefault() : null
-        }, false);
-
         event.preventDefault();
 
         let addForm = _.clone(this.state.addForm);
