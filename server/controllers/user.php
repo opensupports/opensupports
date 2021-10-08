c64f1f1ea66ff141b119ec547175f63b20ca321d
Guillermo Giuliana <guillermogiuliana@hotmail.com>
2021-10-08 19:07:14 -0300

    [DEV-146]  resend verification email backend (#1049)
    * add path resend signup token
    
    * add ruby tests

--- server/controllers/user.php
+++ server/controllers/user.php
@@ -24,6 +24,7 @@ $userControllers->addController(new DisableUserController);
 $userControllers->addController(new EditCustomFieldsController);
 $userControllers->addController(new EditSupervisedListController);
 $userControllers->addController(new GetSupervisedTicketController);
+$userControllers->addController(new ResendEmailTokenController);
 
 $userControllers->addController(new ResendInviteUserController);
 
