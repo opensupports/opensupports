9f9e1dbd91230e9f38686e3aa4bc2c7abd2deebb
Ivan Diaz <ivandiazwm@gmail.com>
2021-10-21 12:09:13 -0300

    Remove textContent check for non-textarea elements (#1066)

--- client/src/lib-app/validations/length-validator.js
+++ client/src/lib-app/validations/length-validator.js
@@ -2,17 +2,24 @@ import Validator from 'lib-app/validations/validator';
 import _ from 'lodash';
 
 class LengthValidator extends Validator {
-    constructor(length, errorKey = 'INVALID_VALUE', validator = null) {
+    constructor(length, errorKey = 'INVALID_VALUE', validator = null, useHTMLDiv = false) {
         super(validator);
 
         this.minlength = length;
         this.errorKey = errorKey;
+        this.useHTMLDiv = useHTMLDiv;
     }
 
     validate(value = '', form = {}) {
-        let div = document.createElement("div");
-        div.innerHTML = value;
-        let text = div.textContent || div.innerText || "";
+        let text;
+        if (this.useHTMLDiv) {
+            let div = document.createElement("div");
+            div.innerHTML = value;
+            text = div.textContent || div.innerText || "";
+        } else {
+            text = value;
+        }
+
         if(_.every(text, c => c === " ")) {
             text = text.replace(/\s/g, '');
         }
