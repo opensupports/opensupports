92eeec88a0fc8e48d146d08516cf4f90de7231d7
LautaroCesso <lautaro_cesso@hotmail.com>
2021-12-02 21:34:57 -0300

    Change some test names

--- tests/ticket/edit-comment.rb
+++ tests/ticket/edit-comment.rb
@@ -7,14 +7,14 @@ describe '/ticket/edit-comment' do
     Scripts.login()
     Scripts.createTicket('ticket made by a user','content of the ticket made by a user')
 
-    def queryToGetTicketCommentsByTicketId(ticketId)
+    def getTicketEventsCommentsQuery(ticketId)
         return "SELECT * FROM `ticketevent` WHERE `type` = 'COMMENT' AND `ticket_id` = #{ticketId} ORDER BY `ticketevent`.`id` ASC;"
     end
 
     ticket = $database.getRow('ticket', 'ticket made by a user', 'title')
     ticket2 = $database.getRow('ticket', 'ticket made by a staff', 'title')
 
-    it 'should change content of the ticket if the author user tries it and the ticket has not any other comment' do
+    it 'should success if author is right and ticket has no comment' do
         result = request('/ticket/edit-comment', {
             csrf_userid: $csrf_userid,
             csrf_token: $csrf_token,
@@ -22,7 +22,7 @@ describe '/ticket/edit-comment' do
             ticketNumber: ticket['ticket_number']
         })
 
-        tickets_comments = $database.query(queryToGetTicketCommentsByTicketId(ticket['id']))
+        tickets_comments = $database.query(getTicketEventsCommentsQuery(ticket['id']))
         tickets_comments.size.should.equal(0)
         ticket = $database.getRow('ticket', 'ticket made by a user', 'title')
 
@@ -30,14 +30,14 @@ describe '/ticket/edit-comment' do
         (ticket['content']).should.equal('content edited by the user')
     end
 
-    it 'should change the content of a comment if the user is the author and it is the last comment' do
-        tickets_comments = $database.query(queryToGetTicketCommentsByTicketId(ticket['id']))
+    it 'should success if author is right and its the last comment' do
+        tickets_comments = $database.query(getTicketEventsCommentsQuery(ticket['id']))
         tickets_comments.size.should.equal(0)
 
         Scripts.commentTicket(ticket['ticket_number'],'com    ment of a user')
         ticketevent = $database.getRow('ticketevent', 'com    ment of a user', 'content')
 
-        tickets_comments = $database.query(queryToGetTicketCommentsByTicketId(ticket['id']))
+        tickets_comments = $database.query(getTicketEventsCommentsQuery(ticket['id']))
         tickets_comments.size.should.equal(1)
         tickets_comments.to_a.last['content'].should.equal('com    ment of a user')
 
@@ -48,7 +48,7 @@ describe '/ticket/edit-comment' do
             ticketEventId: ticketevent['id']
         })
 
-        tickets_comments = $database.query(queryToGetTicketCommentsByTicketId(ticket['id']))
+        tickets_comments = $database.query(getTicketEventsCommentsQuery(ticket['id']))
         tickets_comments.size.should.equal(1)
 
         (result['status']).should.equal('success')
@@ -58,7 +58,7 @@ describe '/ticket/edit-comment' do
         Scripts.commentTicket(ticket['ticket_number'],'com    ment of a user 2')
         ticketevent = $database.getRow('ticketevent', 'com    ment of a user 2', 'content')
 
-        tickets_comments = $database.query(queryToGetTicketCommentsByTicketId(ticket['id']))
+        tickets_comments = $database.query(getTicketEventsCommentsQuery(ticket['id']))
         tickets_comments.size.should.equal(2)
         tickets_comments.to_a.last['content'].should.equal('com    ment of a user 2')
 
@@ -69,7 +69,7 @@ describe '/ticket/edit-comment' do
             ticketEventId: ticketevent['id']
         })
 
-        tickets_comments = $database.query(queryToGetTicketCommentsByTicketId(ticket['id']))
+        tickets_comments = $database.query(getTicketEventsCommentsQuery(ticket['id']))
         tickets_comments.size.should.equal(2)
         tickets_comments.to_a.last['content'].should.equal('comment edited by the user 2')
 
@@ -78,7 +78,7 @@ describe '/ticket/edit-comment' do
         Scripts.commentTicket(ticket['ticket_number'],'com    ment of a user 3')
         ticketevent = $database.getRow('ticketevent', 'com    ment of a user 3', 'content')
 
-        tickets_comments = $database.query(queryToGetTicketCommentsByTicketId(ticket['id']))
+        tickets_comments = $database.query(getTicketEventsCommentsQuery(ticket['id']))
         tickets_comments.size.should.equal(3)
         tickets_comments.to_a.last['content'].should.equal('com    ment of a user 3')
 
@@ -89,7 +89,7 @@ describe '/ticket/edit-comment' do
             ticketEventId: ticketevent['id']
         })
 
-        tickets_comments = $database.query(queryToGetTicketCommentsByTicketId(ticket['id']))
+        tickets_comments = $database.query(getTicketEventsCommentsQuery(ticket['id']))
         tickets_comments.size.should.equal(3)
         tickets_comments.to_a.last['content'].should.equal('comment edited by the user 3')
 
@@ -98,8 +98,8 @@ describe '/ticket/edit-comment' do
         (result['status']).should.equal('success')
     end
 
-    it 'should not change the content of the ticket if the user is the author and the ticket has comments' do
-        tickets_comments = $database.query(queryToGetTicketCommentsByTicketId(ticket['id']))
+    it 'should fail if author is right but ticket has other commets below' do
+        tickets_comments = $database.query(getTicketEventsCommentsQuery(ticket['id']))
         tickets_comments.size.should.equal(3)
 
         result = request('/ticket/edit-comment', {
@@ -111,16 +111,14 @@ describe '/ticket/edit-comment' do
 
         (result['status']).should.equal('fail')
         (result['message']).should.equal('TICKET_CONTENT_CANNOT_BE_EDITED')
-    end
 
-    it 'should not change the content of the ticket comment if the user is the author and the comment is not the last' do
-        tickets_comments = $database.query(queryToGetTicketCommentsByTicketId(ticket['id']))
+        tickets_comments = $database.query(getTicketEventsCommentsQuery(ticket['id']))
         tickets_comments.size.should.equal(3)
         ticket_comment3 = tickets_comments.to_a.last
 
         Scripts.commentTicket(ticket['ticket_number'],'com    ment of a user 4')
 
-        tickets_comments = $database.query(queryToGetTicketCommentsByTicketId(ticket['id']))
+        tickets_comments = $database.query(getTicketEventsCommentsQuery(ticket['id']))
         tickets_comments.size.should.equal(4)
 
         result = request('/ticket/edit-comment', {
@@ -134,7 +132,7 @@ describe '/ticket/edit-comment' do
         (result['message']).should.equal('INVALID_TICKET_EVENT')
     end
 
-    it 'should not change the content of the ticket if the user is not the author and the ticket has not comments' do
+    it 'should fail if ticket has no comments but author is wrong' do
         result = request('/ticket/edit-comment', {
             csrf_userid: $csrf_userid,
             csrf_token: $csrf_token,
@@ -142,7 +140,7 @@ describe '/ticket/edit-comment' do
             ticketNumber: ticket2['ticket_number']
         })
 
-        tickets_comments = $database.query(queryToGetTicketCommentsByTicketId(ticket2['id']))
+        tickets_comments = $database.query(getTicketEventsCommentsQuery(ticket2['id']))
         tickets_comments.size.should.equal(0)
         ticket2 = $database.getRow('ticket', 'ticket made by a staff', 'title')
 
@@ -151,16 +149,16 @@ describe '/ticket/edit-comment' do
         (result['message']).should.equal('NO_PERMISSION')
     end
 
-    it 'should not change the content of the ticket if the user is not the author and the ticket has comments' do
+    it 'should fail if ticket has comment  and author is wrong' do
         Scripts.logout()
         Scripts.login($staff[:email], $staff[:password], true)
 
-        tickets_comments = $database.query(queryToGetTicketCommentsByTicketId(ticket2['id']))
+        tickets_comments = $database.query(getTicketEventsCommentsQuery(ticket2['id']))
         tickets_comments.size.should.equal(0)
 
         Scripts.commentTicket(ticket2['ticket_number'],'com    ment of a staff')
 
-        tickets_comments = $database.query(queryToGetTicketCommentsByTicketId(ticket2['id']))
+        tickets_comments = $database.query(getTicketEventsCommentsQuery(ticket2['id']))
         tickets_comments.size.should.equal(1)
         tickets_comments.to_a.last['content'].should.equal('com    ment of a staff')
 
@@ -174,7 +172,7 @@ describe '/ticket/edit-comment' do
             ticketNumber: ticket2['ticket_number']
         })
 
-        tickets_comments = $database.query(queryToGetTicketCommentsByTicketId(ticket2['id']))
+        tickets_comments = $database.query(getTicketEventsCommentsQuery(ticket2['id']))
         tickets_comments.size.should.equal(1)
         ticket2 = $database.getRow('ticket', 'ticket made by a staff', 'title')
 
@@ -184,8 +182,8 @@ describe '/ticket/edit-comment' do
     end
 
 
-    it 'should not change the content of the ticket comment if the user is not the author and the comment is the last' do
-        tickets_comments = $database.query(queryToGetTicketCommentsByTicketId(ticket2['id']))
+    it 'should fail if author is wrong but comment is the last' do
+        tickets_comments = $database.query(getTicketEventsCommentsQuery(ticket2['id']))
         tickets_comments.size.should.equal(1)
 
         ticket_comment = tickets_comments.to_a.last
@@ -199,7 +197,7 @@ describe '/ticket/edit-comment' do
             ticketEventId: ticket_comment['id']
         })
 
-        tickets_comments = $database.query(queryToGetTicketCommentsByTicketId(ticket2['id']))
+        tickets_comments = $database.query(getTicketEventsCommentsQuery(ticket2['id']))
         tickets_comments.size.should.equal(1)
 
         ticket_comment = tickets_comments.to_a.last
@@ -209,17 +207,17 @@ describe '/ticket/edit-comment' do
         ticket_comment['content'].should.equal('com    ment of a staff')
     end
 
-    it 'should not change the content of the ticket comment if the user is not the author and the comment is not the last' do
+    it 'should fail if the author and comment are wrong' do
         Scripts.logout()
         Scripts.login($staff[:email], $staff[:password], true)
 
-        tickets_comments = $database.query(queryToGetTicketCommentsByTicketId(ticket2['id']))
+        tickets_comments = $database.query(getTicketEventsCommentsQuery(ticket2['id']))
         tickets_comments.size.should.equal(1)
         first_comment = tickets_comments.to_a.last
 
         Scripts.commentTicket(ticket2['ticket_number'],'com    ment of a staff 2')
 
-        tickets_comments = $database.query(queryToGetTicketCommentsByTicketId(ticket2['id']))
+        tickets_comments = $database.query(getTicketEventsCommentsQuery(ticket2['id']))
         tickets_comments.size.should.equal(2)
         last_comment = tickets_comments.to_a.last
         last_comment['content'].should.equal('com    ment of a staff 2')
