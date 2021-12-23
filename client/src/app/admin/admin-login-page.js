c3067417582116e1b2ad4bbebf6fc8d13f912972
LautaroCesso <lautaro_cesso@hotmail.com>
2021-12-23 15:08:24 -0300

    Fix bug in login widget transition

--- client/src/app/admin/admin-login-page.js
+++ client/src/app/admin/admin-login-page.js
@@ -93,7 +93,7 @@ class AdminLoginPage extends React.Component {
 
     renderLoginCaptcha() {
         return(
-            <div className="admin-login-page__captcha">
+            <div className={`main-home-page__${this.props.sitekey ? "captcha" : "no-captcha"}`}>
                 <Captcha ref="captcha" />
             </div>
         )
@@ -240,6 +240,7 @@ class AdminLoginPage extends React.Component {
 
 export default connect((store) => {
     return {
-        session: store.session
+        session: store.session,
+        sitekey: store.config.reCaptchaKey
     };
 })(AdminLoginPage);
