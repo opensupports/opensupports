a527fa277b70289f26a0be75e40e0c7e47330406
Maxi Redigonda <maxiredigonda@gmail.com>
2022-03-07 17:18:47 -0300

    Update code to generate link to uploaded file in ticket/article to use apiRoot

--- server/libs/Controller.php
+++ server/libs/Controller.php
@@ -226,9 +226,11 @@ abstract class Controller {
 
         $imagePaths = [];
         $url = Setting::getSetting('url')->getValue();
+        global $client;
+        $apiRoot = 'https://' . $client->getClientVersionURL();
         for($i=0;$i<$totalImages;$i++) {
             $fileUploader->upload("image_$i");
-            $imagePaths[] = $url . '/api/system/download?file=' . $fileUploader->getFileName();
+            $imagePaths[] = $apiRoot . '/system/download?file=' . $fileUploader->getFileName() . '&client_id=' . $client->getClientId();
         }
 
         return $imagePaths;
