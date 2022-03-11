783207b1d9d18b3e96bf8b8af3fc6610582e91ac
Maxi Redigonda <maxiredigonda@gmail.com>
2022-03-11 10:58:56 -0300

    Revert "Fix name of origin allowlist site"
    This reverts commit dcba02e961dba8cf182be6bf74409e11ccb0a5cf.

--- server/controllers/system/download.php
+++ server/controllers/system/download.php
@@ -40,7 +40,7 @@ class DownloadController extends Controller {
 
     public function handler() {
 
-        $allowed = array('https://maxi.ticketcenter.cloud');
+        $allowed = array('maxi.ticketcenter.cloud');
 
         if(isset($_SERVER['HTTP_ORIGIN']) && in_array($_SERVER['HTTP_ORIGIN'], $allowed)){
             header("Access-Control-Allow-Origin: " . $_SERVER['HTTP_ORIGIN'] . "");
