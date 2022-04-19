861f7dc25480209486deded6bc57a1c796fcc20b
LautaroCesso <59095036+LautaroCesso@users.noreply.github.com>
2022-04-19 19:28:51 -0300

    [DEV-214] Fix email template validation (#1136)
    * Fix email template validation
    
    * Fix bug with default email templates
    
    * Use template codes instead of selected index in text3 email template field
    
    * Fix bug in no email template forms
    
    * Use componentDidUpdate method

--- client/src/core-components/form.js
+++ client/src/core-components/form.js
@@ -39,7 +39,13 @@ class Form extends React.Component {
     }
 
     componentDidMount() {
-        this.setState(this.getInitialFormAndValidations());
+        this.setState(this.getInitialFormAndValidations(this.props.children));
+    }
+
+    componentDidUpdate(nextProps) {
+        if(nextProps.values != this.props.values) {
+            this.setState(this.getInitialFormAndValidations(nextProps.children));
+        }
     }
 
     render() {
@@ -135,12 +141,11 @@ class Form extends React.Component {
         return newErrors;
     }
 
-    getInitialFormAndValidations() {
+    getInitialFormAndValidations(children) {
         let form = {};
         let validations = {};
 
-        reactDFS(this.props.children, (child) => {
-
+        reactDFS(children, (child) => {
             if (this.isValidField(child)) {
                 form[child.props.name] = child.props.value || FormField.getDefaultValue(child.props.field);
 
