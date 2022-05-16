713a5b5ee1a35932e5c62e67f489bb40277330bc
Maximiliano Redigonda <maxiredigonda@gmail.com>
2022-05-16 17:32:50 -0300

    Try to start server before truncate db (#1208)

--- tests/truncate_db.sh
+++ tests/truncate_db.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 # DELETE ALL TABLES
+service mysql start;
 TABLES=$(mysql --host ${MYSQL_HOST} --port ${MYSQL_PORT} -u root development -e "SHOW TABLES IN development;" | awk '{ print $1}' | grep -v '^Tables')
 
 for t in $TABLES
